6fd29fcfda5456c4c5d7c86f135da422d0c18e40
BarbarianTarkus <richvclaros@gmail.com>
2023-04-13 16:18:23 +0200

    ESSAGE

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
-##!/bin/bash
+#!/bin/bash
+
+install_check_apt()
+{
+    if ! command -v "$1" &> /dev/null; then
+        echo "$1 is not installed. Installing..."
+        sudo apt install -y "$1"
+    else
+        echo "$1 is already installed."
+    fi
+}
+
+
+
 
 # Check if zsh is installed
-if ! command -v zsh &> /dev/null; then
-    echo "zsh is not installed. Installing..."
-    sudo apt install -y zsh
-else
-    echo "zsh is already installed."
-fi
+install_check_apt zsh
+
+sleep 1
 
 # Check if oh-my-zsh is installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
@@ -19,15 +29,15 @@ fi
 # Check if powerlevel10k is installed
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
     echo "powerlevel10k is not installed. Installing..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
 else
     echo "powerlevel10k is already installed."
 fi
 
 # Check if antigen is installed
-if ! command -v antigen &> /dev/null; then
+if antigen &> /dev/null; then
     echo "antigen is not installed. Installing..."
-    curl -L git.io/antigen > ~/.antigen.zsh
+    curl -L git.io/antigen > ~/antigen.zsh
 else
     echo "antigen is already installed."
 fi
@@ -41,41 +51,28 @@ else
 fi
 
 # Check if fzf is installed
-if ! command -v fzf &> /dev/null; then
-    echo "fzf is not installed. Installing..."
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install --all
-else
-    echo "fzf is already installed."
-fi
+install_check_apt fzf
+
+
+# Check if bat is installed
+install_check_apt bat
+
 
 # Restore zsh config
 echo "Loading shell config .zshrc"
-cp ~/.zshrc ~/.zshrc.bak
-cp zshrc ~/.zshrc
+cp zshrc.bak ~/.zshrc
 
 # Restore theme config
 echo "Loading theme .p10k.zsh" 
-cp ~/.p10k.zsh ~/.p10k.zsh.bak
-cp p10k.zsh ~/.p10k.zsh
-
-# Load antigen
-source ~/.antigen.zsh
-
-# Use oh-my-zsh
-antigen use oh-my-zsh
-
-# Load plugins
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-autosuggestions
-antigen apply
-EOF
+cp p10k.zsh.bak ~/.p10k.zsh
 
 # Reload zsh config
-source ~/.zshrc
+zsh
 
-sudo apt -y install bat
 
 echo "To browse the Cheatshet repositories of navi, run the command:"
 echo
 echo "navi repo browse"
+
+
+zsh
