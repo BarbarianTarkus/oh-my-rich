9b82a200681ceaee5df2b4c0e6c7b26239aa05fd
Ricardo Verduguez <ricardo@tresipunt.com>
2024-07-30 15:52:39 +0200

    Added dependency check and fixed some installs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
+
+# Definir colores
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' # Sin color
+
+
+
+dependencies=("zsh" "curl" "git")
+
 install_apt() {
     if ! type "$1" &>/dev/null; then
         echo "$1 is not installed. Installing..."
-        if ! sudo apt install -y "$1"; then
+        if sudo apt install -y "$1"; then
             echo "✅ $1 installed successfully."
         else
             echo "❌ $1 installation failed. You can try to install it manually or if you are not on Debian, use your package manager."
-            exit 0
+            exit 1
         fi
     else
         echo "✅ $1 is already installed."
+    fi
+}
+
+install_additional_package() {
+    echo -e "Do you want to install $1? (y/n) ${RED}Only if you have ${YELLOW}apt${RED} as package manager${NC}"
+    read -r response
+    if [ "$response" = "y" ]; then
+        install_apt "$1"
+    else
+        sed -i "/alias $1 /d" $zshrc_file
     fi
 }
 
@@ -25,8 +46,8 @@ install_fzf() {
 }
 
 install_zsh() {
-    if [ -z "$ZSH_VERSION" ]; then
-        echo "⌛zsh is not installed. Installing..."
+    if ! type zsh &>/dev/null; then
+        echo "⌛ zsh is not installed. Installing..."
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         echo "✅ zsh installed successfully."
     else
@@ -37,7 +58,7 @@ install_zsh() {
 install_oh_my_zsh() {
     # Check if oh-my-zsh is installed
     if [ ! -d "$HOME/.oh-my-zsh" ]; then
-        echo "oh-my-zsh is not installed. Installing..."
+        echo -e "oh-my-zsh is not installed. Installing... ${RED} Please exit the shell after installation ${NC}"
         # Installing oh-my-zsh
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     else
@@ -59,10 +80,14 @@ install_powerlevel10k() {
 }
 
 install_zinit() {
-    if ! type zinit &>/dev/null; then
-        echo "⌛zinit could not be found, installing now..."
-        bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-        echo "✅ zinit is installed. You can now use it to install plugins."
+    if type zinit &>/dev/null; then
+        echo "⌛ zinit could not be found, installing now..."
+        if bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"; then
+            echo "✅ zinit is installed. You can now use it to install plugins."
+        else
+            echo "❌ zinit installation failed. Please try to install it manually."
+            exit 1
+        fi
     else
         echo "✅ zinit is already installed."
     fi
@@ -77,9 +102,6 @@ install_navi() {
         echo "✅ navi is already installed."
     fi
 }
-
-
-
 
 check_and_copy() {
     cp -a "$1" "$2" # -p: Preserve all: metadata & attributes
@@ -99,10 +121,18 @@ copy_config_files() {
     check_and_copy $p10k_file "$HOME"/.p10k.zsh
 }
 
-
-
+check_dependency() {
+    if ! command -v "$1" &>/dev/null; then
+        echo -e " ${GREEN}$1${RED} is not installed,${NC} please install it and re-run the installation"
+        exit 0;
+    fi
+}
 
 ## Main
+
+for dep in "${dependencies[@]}"; do
+    check_dependency "$dep"
+done
 
 install_zsh
 
@@ -118,32 +148,10 @@ install_navi
 
 install_fzf
 
-
-echo "Do you want to install bat(only if you have APT)? [y/n]"
-read -r response
-case $response in
-    [Yy]*)
-        install_apt bat
-        ;;
-    *)
-        echo "⏩Skipping bat installation."
-        # Remove alias from zshrc.bak: "alias badcat="cat"
-        sed -i '/alias badcat/d' $zshrc_file
-        ;;
-esac
-
-echo "Do you want to install lsd(only if you have APT)? [y/n]"
-read -r response
-case $response in
-    [Yy]*)
-        install_apt lsd
-        ;;
-    *)
-        echo "⏩Skipping lsd installation."
-        # Remove alias from zshrc.bak
-        sed -i '/alias lsd/d' $zshrc_file
-        ;;
-esac
+additional_packages=("bat" "zoxide")
+for package in "${additional_packages[@]}"; do
+    install_additional_package "$package"
+done
 
 # reload zsh
 zsh
